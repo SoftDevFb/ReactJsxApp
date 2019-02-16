13fe1dcc771f725b7ddf828965e2f502fbe3755d
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2019-02-15 18:46:47 -0600

    Left off at start of Section 3 Udemy Modern React and Redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 //Create a React component
 const App = () => {
-    return <div>Hi there!</div>
+    const buttonText =  { text: 'Click Me' };
+    const labelText = { text: 'Enter Name: '}; 
+
+    return (
+    <div>
+        <label htmlFor="name">{labelText.text}</label>
+        <input id="name" type="text" className="label" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {buttonText.text}
+        </button>
+    </div>
+    );
 };
 
 //Take the react component and show it on the screen
